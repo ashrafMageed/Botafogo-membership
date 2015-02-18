022b7239808560313d78c32160b3026dd1680d0e
ashrafMageed <ashraf_mageed@hotmail.com>
2015-02-18 14:10:08 +0000

    Initial commit

---
+++ Gruntfile.js
@@ -0,0 +1,10 @@
+module.exports = function(grunt){
+
+    grunt.initConfig({
+        //jshint :{
+        //    files: ['lib/**/*js', 'models/**/*.ja']
+        //}
+    });
+
+    //grunt.loadNpmTasks("grunt-contrib-jshint");
+}
